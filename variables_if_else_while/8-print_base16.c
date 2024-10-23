73acc18e2d408871d8522925a2e0a82ea7f39e1f
Kadiga6 <9510@holbertonstudents.com>
2024-10-23 22:05:46 +0000

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
+
 /**
   *main - Entry point
   *
   * Return:Always 0(Sucess)
   */
 
-
 int main(void)
 {
+int num;
 char letter;
-for (letter = 'z'; letter >= 'a'; letter--)
-{
+
+for (num = 0; num <= 9; num++)
+putchar(num + '0');
+
+for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
-}
+
 putchar('\n');
 
 return (0);
