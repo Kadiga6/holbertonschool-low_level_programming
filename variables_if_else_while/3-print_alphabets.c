30080e8ea3ef7fe5f6414cdb7b40051756e6b575
Kadiga6 <9510@holbertonstudents.com>
2024-10-23 14:32:41 +0000

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,16 @@
 
 int main(void)
 {
-char letter;
-
-for (letter = 'a'; letter <= 'z'; letter++)
+char c;
+for (c = 'a'; c <= 'z'; c++)
 {
-putchar(letter);
+putchar(c);
 }
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar(c);
+}
+
 putchar('\n');
 
 return (0);
