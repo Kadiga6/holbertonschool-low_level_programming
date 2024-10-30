a1262e643b17d9bc655e4657dcc24ed5b99e7525
root <root@LAPTOP-3F3BGL77.>
2024-10-30 17:18:00 +0100

    3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Prints a string followed by a new line.
+ * @str: The string to be printed.
+ */
+
+void _puts(char *str)
+{
+while (*str)
+{
+putchar(*str);
+str++;
+}
+putchar('\n');
+}
