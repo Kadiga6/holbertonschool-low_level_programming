b84ae21d4d373b6109fc62f3e79e0d5b4e856a01
root <root@LAPTOP-3F3BGL77.>
2024-11-04 22:59:20 +0100

    5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -6,12 +6,14 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-char str[] = "Look up!\n";
-char *ptr;
-ptr = string_toupper(str);
-printf("%s", ptr);
-printf("%s", str);
+char *s = "hello, world";
+char *f = "world";
+char *t;
+
+t = _strstr(s, f);
+printf("%s\n", t);
 return (0);
 }
