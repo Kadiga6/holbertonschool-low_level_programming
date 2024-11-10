73c6249d3bd539bd23ecbc30d82ab5c8f868c229
root <root@LAPTOP-3F3BGL77.>
2024-11-10 21:46:25 +0100

    3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints all arguments it receives
+* @argc: variable
+* @argv: variable to check
+* Return: always zero
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+printf("%d\n", num1  *num2);
+return (0);
+}
