cc3f4b422739bae11e40a718e9e4de34e87d50f4
root <root@LAPTOP-3F3BGL77.>
2024-11-17 21:57:17 +0100

    0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure representing a dog
+ *@name: name of dog (type char)
+ *@age: age of dog (type float)
+ *@owner: name of dog owner (type char)
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
