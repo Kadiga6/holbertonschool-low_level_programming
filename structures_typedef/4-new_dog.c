80473ed228896eae262e5b1e92be761aec30c7a8
root <root@LAPTOP-3F3BGL77.>
2024-11-17 23:54:25 +0100

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,34 +1,35 @@
-#include "dog.h"
-#include <stdio.h>
 #include <string.h>
+#include "dog.h"
 #include <stdlib.h>
 /**
- * new_dog - creates a new dog
- * @name: The name of dog (type char)
- * @age: The age of dog (type float)
- * @owner: The owner of the dog
- * Return: A pointer to the new dog (dog_t) structure, or NULL if it fails.
- */
-
+* new_dog -  function that creates a new dog
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
+* Return: pointer to the new dog
+*/
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new_dog_ptr = malloc(sizeof(dog_t));
-if (new_dog_ptr == NULL)
-	return (NULL);
+	dog_t *newDog;
 
-new_dog_ptr->name = malloc(strlen(name) + 1);
-new_dog_ptr->owner = malloc(strlen(owner) + 1);
-
-if (new_dog_ptr->name == NULL || new_dog_ptr->owner == NULL)
-{
-free(new_dog_ptr->name);
-free(new_dog_ptr->owner);
-free(new_dog_ptr);
-return (NULL);
-}
-strcpy(new_dog_ptr->name, name);
-strcpy(new_dog_ptr->owner, owner);
-new_dog_ptr->age = age;
-
-return (new_dog_ptr);
+	if (name == NULL || owner == NULL)
+	{
+		return (NULL);
+	}
+	newDog = malloc(sizeof(dog_t));
+	if (newDog == NULL)
+	{
+		return (NULL);
+	}
+	newDog->name = strdup(name);
+	newDog->owner = strdup(owner);
+	if (newDog->name == NULL || newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog->owner);
+		free(newDog);
+		return (NULL);
+	}
+	newDog->age = age;
+	return (newDog);
 }
