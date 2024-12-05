6db6df7f225fd9fbea0e87d3111fb5a021adb762
root <root@LAPTOP-3F3BGL77.>
2024-12-05 13:23:04 +0100

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,13 +10,15 @@
 size_t print_list(const list_t *h)
 {
 size_t node_count = 0;
-while (h =! NULL)
+while (h != NULL)
 {
 	if (h->str == NULL)
 		printf ("[0] (nil)");
 else
+{
 	printf("[%u] %s\n", h->len, h->str);
-h= -h *next;
+}
+h= h->next;
 node_count++;
 }
 return (node_count);
